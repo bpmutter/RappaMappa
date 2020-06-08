b6e6246e63733cfe0bac5dd2b7ffd1059e45831f
Ben Perlmutter <benperlmutter94@gmail.com>
2020-06-08 12:52:09 -0500

    Updates

---
+++ precache-manifest.e3076a9f1acbb898e9454724f2d907ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "869272244d9c1bbf224b45d6f18e21bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "031b4125634dfcabbf55",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dad9574b2fd66a0d86b3",
+    "url": "/static/js/2.c8babcbe.chunk.js"
+  },
+  {
+    "revision": "abe457e23a52f26e39e228fd8cf29cc4",
+    "url": "/static/js/2.c8babcbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031b4125634dfcabbf55",
+    "url": "/static/js/main.f29e96c2.chunk.js"
+  },
+  {
+    "revision": "f9a2526f1c5d1f34e957",
+    "url": "/static/js/runtime-main.e0d4ced6.js"
+  }
+]);
