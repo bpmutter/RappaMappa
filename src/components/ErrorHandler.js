bc44c1ddfe03534b6e4afa8bc39cb432c5b08bfe
Ben Perlmutter <benperlmutter94@gmail.com>
2020-06-09 17:00:47 -0500

    clean up error handler

--- src/components/ErrorHandler.js
+++ src/components/ErrorHandler.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import { makeStyles } from "@material-ui/core/styles";
@@ -20,7 +19,6 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function ErrorHandler(props) {
-  console.log('ERRORS IN HANDLER::', props.numErrors)
   const classes = useStyles();
 
   const handleClose = (event, reason) => {
@@ -32,25 +30,24 @@ function ErrorHandler(props) {
 
   return (
     <>
-    {props.numErrors ? (<div className={classes.root}>
-        {props.errors.map( (error, i) =>{
-           return (
-            <Snackbar open={true} autoHideDuration={6000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="error">
-                    {error}
-                </Alert>            
-            </Snackbar>)
-        })}
-        
+        {props.numErrors ? (<div className={classes.root}>
+            {props.errors.map( (error, i) =>{
+            return (
+                <Snackbar open={true} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="error">
+                        {error}
+                    </Alert>            
+                </Snackbar>)
+            })}
+            
 
-    </div>) : null}
+        </div>) : null}
     </>
     
   );
 }
 
 const mapStateToProps = (state) => {
-    console.log("HOW MANY ERRORS::", state.errorHandler.errors.length);
   return {
     errors: state.errorHandler.errors,
     numErrors: state.errorHandler.errors.length,
@@ -61,61 +58,4 @@ const mapDispatchToProps = (dispatch) => {
     clearErrorMessages: () => dispatch(clearErrorMessages()),
   };
 };
-
 export default connect(mapStateToProps, mapDispatchToProps)(ErrorHandler);
-
-// import React, {useEffect, useState} from "react";
-// import Button from "@material-ui/core/Button";
-// import Snackbar from "@material-ui/core/Snackbar";
-// import {connect} from 'react-redux';
-// import {clearErrorMessages} from '../store/errorHandler';
-// import { makeStyles } from "@material-ui/core/styles";
-// import SnackbarContent from "@material-ui/core/SnackbarContent";
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     maxWidth: 600,
-//     "& > * + *": {
-//       marginTop: theme.spacing(2),
-//     },
-//     zIndex: 20
-//   },
-// }));
-
-// function ErrorHandler(props) {
-//   const classes = useStyles();
-//   const [isOpen, setOpen] = useState(true);
-
-
-//   useEffect(()=>{
-//     // console.log("ERRORS::", props.errors);
-//     if(props.errors.length){
-//         setOpen(true);
-//     }    
-//   },);
-
-// //   const handleClick = () => {
-// //       setState({open: true})
-// //   }
-
-
-//   const handleClose = () => {
-//     setOpen(false);
-//     props.clearErrorMessages();
-//   };
-
-//   return (
-//     <>
-//     <div>
-//       <SnackbarContent
-//         open={isOpen}
-//         className={classes.root}
-//         onClose={handleClose}
-//         message="I love snacks"
-//       />
-//     </div>
-//     </>
-//   );
-// }
-
-
