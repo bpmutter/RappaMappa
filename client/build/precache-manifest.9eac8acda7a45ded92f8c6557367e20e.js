923e088fd7521f19e98c7cce7b0671bff491d625
Ben Perlmutter <benperlmutter94@gmail.com>
2020-06-10 18:20:48 -0500

    remove build folder from .gitignore

---
+++ client/build/precache-manifest.9eac8acda7a45ded92f8c6557367e20e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dff99519aa378a004841fb4f90ef888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9b7c10a9bde9fde6b51",
+    "url": "/static/css/main.69a50f2f.chunk.css"
+  },
+  {
+    "revision": "0f819702ff1ccf7c8321",
+    "url": "/static/js/2.f87c1515.chunk.js"
+  },
+  {
+    "revision": "abe457e23a52f26e39e228fd8cf29cc4",
+    "url": "/static/js/2.f87c1515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b7c10a9bde9fde6b51",
+    "url": "/static/js/main.d7d42c6f.chunk.js"
+  },
+  {
+    "revision": "f9a2526f1c5d1f34e957",
+    "url": "/static/js/runtime-main.e0d4ced6.js"
+  }
+]);
